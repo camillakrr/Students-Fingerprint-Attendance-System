df155f5dcb0dd34b7f827d2c80e8796750590443
camillakrr <camilla111@ya.ru>
2019-04-25 20:17:22 +0300

    generate attendance

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ def create_students(db):
         last_name = names.get_last_name()
         year = random.randint(1, 4)
         group = random.randint(1, 10)
-        fingerprint = secrets.token_hex(nbytes=16)
-        fingerprints.append(fingerprint)
+        fingerprint_hash = secrets.token_hex(nbytes=16)
+        fingerprints.append(fingerprint_hash)
         students.put({'first_name': first_name, 'last_name': last_name, 'year': year, 'group': group,
-                      'fingerprint': fingerprint},
+                      'fingerprint_hash': fingerprint_hash},
                      auto_commit=False)
     students.commit()
     return fingerprints
 
 
-def create_readers(db):
-    readers = db['readers']
+def create_scanners(db):
+    scanners = db['scanners']
     serial_numbers = []
     for i in range(100, 200):
         serial_number = random.randint(10000000000, 99999999999)
         serial_numbers.append(serial_number)
-        readers.put({'room': i, 'serial_number': serial_number}, auto_commit=False)
-    readers.commit()
+        scanners.put({'classroom': i, 'serial_number': serial_number}, auto_commit=False)
+    scanners.commit()
     return serial_numbers
 
 
-def create_timetable(db):
-    timetable = db['timetable']
+def create_classes(db):
+    classes = db['classes']
     courses = ['Discrete Math and Logic', 'Data Structure and Algorithms', 'Analitical Geometry and Linear Algebra',
                'English', 'Introduction to Programming', 'Mathematical Analysis', 'Introduction to AI',
                'Data Modeling and Databases', 'Software Project', 'Probability and Statistics', 'Networks',
@@ -45,42 +45,45 @@ def create_timetable(db):
     types = ['lecture', 'tutorial', 'lab']
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
     timeslots = ['9:00', '10:35', '12:10', '14:10', '15:45', '17:20', '18:55', '20:30']
-    rooms = dict()
+    classrooms = dict()
     groups = dict()
     for i in range(1000):
         course = secrets.choice(courses)
         type = secrets.choice(types)
-        room = random.randint(100, 200)
+        classroom = random.randint(100, 200)
         year = random.randint(1, 4)
         group = random.randint(1, 10)
         weekday = secrets.choice(weekdays)
-        time = secrets.choice(timeslots)
-        timeSplit = time.split(":")
-        ts = int(timeSplit[0]) * 60 + int(timeSplit[1])
+        timeslot = secrets.choice(timeslots).split(":")
+        hours = timeslot[0]
+        minutes = timeslot[1]
         if type == "lab":
-            if weekday + time + str(room) not in rooms and weekday + time + str(year) + str(group) not in groups:
-                rooms[weekday + time + str(room)] = "true"
-                groups[weekday + time + str(year) + str(group)] = "true"
-                timetable.put(
-                    {"course": course, "type": type, "room": room, "year": year, "groups": "{" + str(group) + "}",
-                     "weekday": weekday, "time": time, "ts": ts})
+            if weekday + hours + minutes + str(classroom) not in classrooms and weekday + hours + minutes + str(
+                    year) + str(group) not in groups:
+                classrooms[weekday + hours + minutes + str(classroom)] = "true"
+                groups[weekday + hours + minutes + str(year) + str(group)] = "true"
+                classes.put(
+                    {"course": course, "type": type, "classroom": classroom, "year": year,
+                     "groups": "{" + str(group) + "}",
+                     "weekday": weekday, "start_time": {"hours": hours, "minutes": minutes}})
         else:
-            if weekday + time + str(room) not in rooms:
-                for i in range(10):
-                    if weekday + time + str(year) + str(i + 1) in groups:
+            if weekday + hours + minutes + str(classroom) not in classrooms:
+                for j in range(10):
+                    if weekday + hours + minutes + str(year) + str(j + 1) in groups:
                         break
-                if i == 9:
-                    rooms[weekday + time + str(room)] = "true"
-                    for j in range(10):
-                        groups[weekday + time + str(year) + str(j + 1)] = "true"
-                    timetable.put(
-                        {"course": course, "type": type, "room": room, "year": year, "groups": "{1,2,3,4,5,6,7,8,9,10}",
-                         "weekday": weekday, "time": time, "ts": ts})
+                if j == 9:
+                    classrooms[weekday + hours + minutes + str(classroom)] = "true"
+                    for q in range(10):
+                        groups[weekday + hours + minutes + str(year) + str(q + 1)] = "true"
+                    classes.put(
+                        {"course": course, "type": type, "classroom": classroom, "year": year,
+                         "groups": "{1,2,3,4,5,6,7,8,9,10}",
+                         "weekday": weekday, "start_time": {"hours": hours, "minutes": minutes}})
 
 
 def create_attendance(db, fingerprints, serial_numbers):
     students = db['students']
-    readers = db['readers']
+    scanners = db['scanners']
     timetable = db['timetable']
     attendance = db['attendance']
     for i in range(100000):
@@ -90,8 +93,8 @@ def create_attendance(db, fingerprints, serial_numbers):
         year = student.json['year']
         group = student.json['group']
         serial_number = secrets.choice(serial_numbers)
-        reader = readers.query("json->>'serial_number'='" + str(serial_number) + "'")[0]
-        room = reader.json['room']
+        scanner = scanners.query("json->>'serial_number'='" + str(serial_number) + "'")[0]
+        room = scanner.json['room']
         unixtime = random.randint(1493078400, 1587772800)
         time = datetime.utcfromtimestamp(unixtime).strftime('%d.%m.%Y %H:%M')
         weekday = datetime.fromtimestamp(unixtime).strftime("%A")
@@ -111,15 +114,17 @@ def create_attendance(db, fingerprints, serial_numbers):
                 lateness = ts - start_time
             attendance.put(
                 {"student_id": student_id, "class_id": class_id, "time": time, "lateness": lateness})
+        if len(timeslot) > 1:
+            print("blya")
 
 
 def main():
     client = PostgresNoSQLClient(host='localhost', port='8000', user='postgres', password='qweqweqwe')
     db = client['attendance_system']
     fingerprints = create_students(db)
-    serial_numbers = create_readers(db)
+    serial_numbers = create_scanners(db)
     create_timetable(db)
-    create_attendance(db, fingerprints, serial_numbers)
+    # create_attendance(db, fingerprints, serial_numbers)
 
 
 if __name__ == '__main__':
