5f96798c1ec3478bd32c316d2734c4ada7760012
mintyque <bochkaryov.pavlodar@gmail.com>
2019-04-26 07:41:18 +0300

    queries

--- main.py
+++ main.py
@@ -118,13 +118,110 @@ def create_attendance(db, fingerprints, serial_numbers):
                 {"student_id": student_id, "class_id": class_id, "date": date, "status": status})
 
 
+'''
+select all late ppl
+'''
+
+
+def query1(db):
+    table = db['attendance']
+    file = open("file1", "w")
+    output = table.query("json->>'status'='late'")
+    file.write(str(len(output)))
+    file.write(" late people in total.\n")
+    for row in output:
+        file.write(str(row))
+        file.write("\n")
+    file.close()
+
+
+'''
+select ppl who are not late
+'''
+
+
+def query2(db):
+    table = db['attendance']
+    file = open("file2", "w")
+    output = table.query("json->>'status'='in time'")
+    file.write(str(len(output)))
+    file.write(" people came on time.\n")
+    for row in output:
+        file.write(str(row))
+        file.write("\n")
+    file.close()
+
+
+'''
+select only ppl who are late by more than 10 minutes
+'''
+
+
+def query3(db):
+    file = open("file3", "w")
+    str1 = "select id as id, json->>'student_id' as student, json->>'class_id' as class, json->>'date' as date, json->>'status' as status from attendance where json->>'status' = 'late'"
+    str2 = "select id as id, json->>'course' as course, json->>'type' as type, json->'start_time'->>'hours' as hours, json->'start_time'->>'minutes' as minutes from classes"
+    str3 = "abs(extract(hour from cast(a.date as date)) * 60 + extract(minute from cast(a.date as date)) - cast(c.hours as int) * 60 + cast(c.minutes as int)) > 10"
+    query = "select * from ({}) a inner join ({}) c on cast(a.class as int) = c.id and({})".format(str1, str2, str3)
+    output = db.execute(query)
+    file.write(str(len(output)))
+    file.write(" people late by more than 10 minutes.\n")
+    for row in output:
+        file.write(str(row))
+        file.write("\n")
+    file.close()
+
+
+'''
+show popularity of each type of class for each course
+'''
+
+
+def query4(db):
+    file = open("file4", "w")
+    str1 = "select id as id, json->>'student_id' as student, json->>'class_id' as class from attendance"
+    str2 = "select id as id, json->>'course' as course, json->>'type' as type from classes"
+    query0 = "select * from ({}) a inner join ({}) c on cast(a.class as int) = c.id".format(str1, str2)
+    query = "select course, type, count(*) as amount from ({}) as joined group by course, type order by course, amount desc".format(query0)
+    output = db.execute(query)
+    file.write("Popularity of classes:\n")
+    for row in output:
+        file.write(str(row))
+        file.write("\n")
+    file.close()
+
+
+'''
+average amount of people present
+'''
+
+
+def query5(db):
+    file = open("file5", "w")
+    str1 = "select id as id, json->>'student_id' as student, json->>'class_id' as class from attendance"
+    str2 = "select id as id, json->>'course' as course, json->>'type' as type from classes"
+    query0 = "select * from ({}) a inner join ({}) c on cast(a.class as int) = c.id".format(str1, str2)
+    query = "select round(avg(amount), 0) from (select course, type, count(*) as amount from ({}) as joined group by course, type) as amount".format(query0)
+    output = db.execute(query)
+    file.write("Average amount of people among all classes:\n")
+    for row in output:
+        file.write(str(row))
+        file.write("\n")
+    file.close()
+
+
 def main():
-    client = PostgresNoSQLClient(host='localhost', port='8000', user='postgres', password='qweqweqwe')
+    client = PostgresNoSQLClient(host='localhost', port='5432', user='postgre', password='postgre')
     db = client['attendance_system']
-    fingerprints = create_students(db)
-    serial_numbers = create_scanners(db)
-    create_classes(db)
-    create_attendance(db, fingerprints, serial_numbers)
+    #fingerprints = create_students(db)
+    #serial_numbers = create_scanners(db)
+    #create_classes(db)
+    #create_attendance(db, fingerprints, serial_numbers)
+    query1(db)
+    query2(db)
+    query3(db)
+    query4(db)
+    query5(db)
 
 
 if __name__ == '__main__':
